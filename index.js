943755870e5984a9a05fad7a3d1495c5183b5d26
= <darioe.barboza@gmail.com>
2021-07-14 00:00:15 -0300

    Ajustes para deployment

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 // Puerto de la app
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 // Importar rutas
 app.use('/api/users', require('./routes/users') );
@@ -25,6 +25,6 @@ app.use('/api/projects', require('./routes/projects') );
 app.use('/api/tasks', require('./routes/tasks'))
 
 // Arrancar la app
-app.listen(PORT, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`El servidor está funcionando en el puerto ${PORT}`)
 })
